f7bec5a505a9b8f25a04187a410f9f04b7f40227
DorDob <64862484+DorDob@users.noreply.github.com>
2020-05-11 14:02:24 +0200

    Add files via upload

---
+++ 1_rozwiazanie.py
@@ -0,0 +1,52 @@
+# Zadanie 1 i 2
+print('Dog')
+
+favourite_car = 'Porsche'
+print(favourite_car)
+
+age = 31
+print(age)
+
+# Zadanie 3
+x = 'Cześć'
+y = 'wszystkim!' 
+print(x, y, sep=', ')
+
+# Zadanie 4 
+print('Moje wymarzone auto to "' + favourite_car + '"')
+
+# Zadanie 5
+print("Roses are red, violets are blue\nUnexpected ';' on line 32.")
+
+# Zadanie 6
+print('    *\n   * *\n  *   *\n *     *\n*       *\n***   ***\n  *   *\n  *   *\n  *****\n')
+#lub
+print(
+"""
+    *
+   * *
+  *   *
+ *     *
+*       *
+***   ***
+  *   *
+  *   *
+  *****
+"""
+)
+
+# Zadanie 7
+a = 3.0 
+b = 4.0 
+c = (2 + a * 2 + b * 2) * 0.5 
+print("c =", c) 
+
+# Zdanie 8
+kilometry = 12.25 
+mile = 7.38 
+
+mile_na_kilometry = mile / 1.61 
+kilometry_na_mile = kilometry * 1.61 
+ 
+print(mile, "mil to", round(mile_na_kilometry, 2), "kilometrów") 
+print(kilometry, "kilometrów to", round(kilometry_na_mile, 2), "mil") 
